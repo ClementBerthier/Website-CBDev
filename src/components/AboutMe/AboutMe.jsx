5e6ca237a26ee856f623a71f2df38574e5e134c5
ClementBerthier <berthierclementpro@gmail.com>
2024-05-15 21:36:06 +0200

    add element of aboutMe section

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -3,13 +3,80 @@ import "./aboutMe.css";
 export default function AboutMe() {
     return (
         <div className="aboutMe" id="aboutMe">
-            <h1>Qui somme nous?</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-                corrupti illum inventore officia quia commodi maxime dolore
-                atque harum ex ipsum assumenda quos accusantium magni, et nam
-                suscipit, cum amet!
-            </p>
+            <div className="description_container">
+                <h2 className="description_title">Qui somme nous?</h2>
+
+                <p className="description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Autem ipsum doloremque error fugit id voluptatem illum? Quam
+                    provident veniam accusantium a cupiditate culpa excepturi,
+                    laudantium voluptate officiis magnam nihil quia.
+                </p>
+            </div>
+            <div className="aboutMe_container">
+                <h2 className="title">Notre principales technonogies</h2>
+                <div className="tech_container">
+                    <div className="firstSection">
+                        <img
+                            className="techLogo"
+                            src="../../src/assets/images/techLogo/HTML5.png"
+                            alt="HTML logo"
+                        />
+                        <img
+                            className="techLogo"
+                            src="../../src/assets/images/techLogo/css3.svg"
+                            alt="CSS logo"
+                        />
+                        <img
+                            className="techLogo"
+                            src="../../src/assets/images/techLogo/JavaScript.png"
+                            alt="Javascript logo"
+                        />
+                        <img
+                            className="techLogo"
+                            src="../../src/assets/images/techLogo/TypeScript.png"
+                            alt="Typescript logo"
+                        />
+                        <img
+                            className="techLogo"
+                            src="../../src/assets/images/techLogo/react.png"
+                            alt="React logo"
+                        />
+                    </div>
+                    <div className="secondSection">
+                        <img
+                            className="techLogo"
+                            src="../../src/assets/images/techLogo/postgre.png"
+                            alt="PostgreSQL logo"
+                        />
+                        <img
+                            className="techLogo"
+                            src="../../src/assets/images/techLogo/node.svg"
+                            alt="Node.JS logo"
+                        />
+                        <img
+                            className="techLogo"
+                            src="../../src/assets/images/techLogo/graphQL.png"
+                            alt="GraphQl logo"
+                        />
+                        <img
+                            className="techLogo"
+                            src="../../src/assets/images/techLogo/github.png"
+                            alt="Git logo"
+                        />
+                        <img
+                            className="techLogo"
+                            src="../../src/assets/images/techLogo/Socket-io.png"
+                            alt="Socket.IO logo"
+                        />
+                        <img
+                            className="techLogo"
+                            src="../../src/assets/images/techLogo/Vite.png"
+                            alt="Vite logo"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }

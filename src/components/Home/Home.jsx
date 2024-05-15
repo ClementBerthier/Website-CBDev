907e310bf38aefd6271f1d5f40b0dd154acee9fb
ClementBerthier <berthierclementpro@gmail.com>
2024-05-15 14:59:19 +0200

    ajust some element responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,26 +8,29 @@ export default function Home() {
             <Header />
             <main>
                 <section className="home">
-                    <div className="picture_container">
-                        <img
-                            className="homePicture"
-                            src={Picture}
-                            alt="Desk Picture"
-                        />
-                    </div>
-                    <div className="title_container">
-                        <h1 className="title">
-                            Clément Berthier{" "}
-                            <span className="spanColor">Développeur</span>
-                        </h1>
-                        <h2 className="subTitle">
-                            Votre <span className="spanColor">vision</span>,
-                            notre
-                            <span className="spanColor"> création</span>
-                        </h2>
-                        <p className="description">
-                            Développement de site web et d{"'"}application web
-                        </p>
+                    <div className="first_container">
+                        <div className="picture_container">
+                            <img
+                                className="homePicture"
+                                src={Picture}
+                                alt="Desk Picture"
+                            />
+                        </div>
+                        <div className="title_container">
+                            <h1 className="title">
+                                Clément Berthier{" "}
+                                <span className="spanColor">Développeur</span>
+                            </h1>
+                            <h2 className="subTitle">
+                                Votre <span className="spanColor">vision</span>,
+                                notre
+                                <span className="spanColor"> création</span>
+                            </h2>
+                            <p className="description">
+                                Développement de site web et d{"'"}application
+                                web
+                            </p>
+                        </div>
                     </div>
                     <div className="wave_container">
                         <svg

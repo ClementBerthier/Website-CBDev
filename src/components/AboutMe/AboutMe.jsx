7bd2b5776f602bf0e771d37a8b20361991212aa7
ClementBerthier <berthierclementpro@gmail.com>
2024-05-30 17:47:16 +0200

    responsive part 1

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -14,64 +14,66 @@ export default function AboutMe() {
         slidesToShow: 4,
         cssEase: "linear",
     };
-    //Todo: regler le probleme d'espacement entre le titre et le texte de la description pour les tres grand ecran
 
     return (
         <>
             <div className="aboutMe" id="aboutMe">
                 <h2 className="aboutMe_title">À propos de nous</h2>
                 <div className="banner_container">
-                    <div className="description_container">
-                        <img
-                            className="logo"
-                            src="/images/user-scan.svg"
-                            alt="logo scan"
-                        />
-                        <div className="text_container">
-                            <div className="description">
-                                <p>
-                                    <span className="spanTitle">
-                                        Qui suis-je ?
-                                    </span>
-                                    <br />
-                                    <br />
-                                    Je m{"'"}appelle Clément Berthier, je suis
-                                    un développeur spécialisé dans la création
-                                    de projets web sur mesure. Passioné par mon
-                                    métier, je travaille autant coté{" "}
-                                    <span>Frontend</span> que{" "}
-                                    <span>Backend</span> pour vous offrir des
-                                    solutions complètes et adaptées à vos
-                                    besoins.
-                                </p>
+                    <div className="container_parent">
+                        <div className="description_container">
+                            <img
+                                className="logo"
+                                src="/images/user-scan.svg"
+                                alt="logo scan"
+                            />
+                            <div className="text_container">
+                                <div className="description">
+                                    <p>
+                                        <span className="spanTitle">
+                                            Qui suis-je ?
+                                        </span>
+                                        <br />
+                                        <br />
+                                        Je m{"'"}appelle Clément Berthier, je
+                                        suis un développeur spécialisé dans la
+                                        création de projets web sur mesure.
+                                        Passioné par mon métier, je travaille
+                                        autant coté <span>Frontend</span> que{" "}
+                                        <span>Backend</span> pour vous offrir
+                                        des solutions complètes et adaptées à
+                                        vos besoins.
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="description_container">
-                        <img
-                            className="logo"
-                            src="/images/code.svg"
-                            alt="logo scan"
-                        />
-                        <div className="text_container">
-                            <div className="description">
-                                <p>
-                                    <span className="spanTitle">
-                                        Que faisons nous ?
-                                    </span>
-                                    <br />
-                                    <br />
-                                    Clément Berthier Développeur est votre
-                                    solution pour la création de vos projets
-                                    web. En tant que professionnel indépendant,
-                                    je mets à votre disposition mon expertise et
-                                    mon expérience dans le développement web et
-                                    d{"'"}applications. Je m{"'"}engage à vous
-                                    accompagner à chaque étape de votre projet,
-                                    en mettant mon savoir-faire et ma passion au
-                                    service de vos idées pour les transformer en
-                                    réalité.
-                                </p>
+                        <div className="description_container">
+                            <img
+                                className="logo"
+                                src="/images/code.svg"
+                                alt="logo scan"
+                            />
+                            <div className="text_container">
+                                <div className="description">
+                                    <p>
+                                        <span className="spanTitle">
+                                            Que faisons nous ?
+                                        </span>
+                                        <br />
+                                        <br />
+                                        Clément Berthier Développeur est votre
+                                        solution pour la création de vos projets
+                                        web. En tant que professionnel
+                                        indépendant, je mets à votre disposition
+                                        mon expertise et mon expérience dans le
+                                        développement web et d{"'"}applications.
+                                        Je m{"'"}engage à vous accompagner à
+                                        chaque étape de votre projet, en mettant
+                                        mon savoir-faire et ma passion au
+                                        service de vos idées pour les
+                                        transformer en réalité.
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
                         </div>
                         <div className="title_container">
                             <h2 className="title">
-                                Clément Berthier{" "}
+                                Clément Berthier <br />
                                 <span className="spanColor">Développeur</span>
                             </h2>
                             <h2 className="subTitle">
@@ -39,3 +39,154 @@ export default function Home() {
         </>
     );
 }
+
+/*
+
+
+.home {
+    z-index: 1;
+    height: 100vh;
+    background-color: rgb(255, 255, 255);
+    .first_container {
+        position: relative;
+        .picture_container {
+            .homePicture {
+                width: 100%;
+                height: 100vh;
+                object-fit: cover;
+            }
+        }
+        .title_container {
+            width: 100%;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: white;
+            .title {
+                font-size: 5vw;
+                font-weight: 700;
+                text-align: center;
+                text-shadow: 0 0 3rem rgba(0, 0, 0, 0.398);
+            }
+            .subTitle {
+                font-size: 2.9vw;
+                font-weight: 500;
+                text-align: center;
+                text-shadow: 0 0 1rem rgba(0, 0, 0, 0.398);
+                margin: 0.6em 0;
+            }
+            .description {
+                font-size: 1.3vw;
+                font-weight: 200;
+                text-align: center;
+                text-shadow: 0 0 1rem rgba(0, 0, 0, 0.398);
+            }
+            .spanColor {
+                color: #2e5339;
+            }
+        }
+    }
+}
+
+*/
+
+/*
+.home {
+    z-index: 1;
+    width: 100vw;
+    background-color: rgb(255, 255, 255);
+    .first_container {
+        position: relative;
+        .picture_container {
+            .homePicture {
+                width: 100%;
+                object-fit: cover;
+            }
+        }
+        .title_container {
+            width: 100%;
+            position: absolute;
+            top: 38%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: white;
+            .title {
+                font-size: 4vw;
+                font-weight: 700;
+                text-align: center;
+                margin: 1.5rem 0;
+
+                text-shadow: 0 0 3rem rgba(0, 0, 0, 0.398);
+            }
+            .subTitle {
+                font-size: 2.5vw;
+                font-weight: 500;
+                text-align: center;
+                text-shadow: 0 0 1rem rgba(0, 0, 0, 0.398);
+                margin: 0.5em 0;
+            }
+            .description {
+                font-size: 1.2vw;
+                font-weight: 200;
+                text-align: center;
+                text-shadow: 0 0 1rem rgba(0, 0, 0, 0.398);
+            }
+            .spanColor {
+                color: #2e5339;
+            }
+        }
+    }
+}
+
+
+*/
+
+/* 
+.home {
+    z-index: 1;
+    height: 100vh;
+    background-color: rgb(255, 255, 255);
+    .first_container {
+        position: relative;
+        .picture_container {
+            .homePicture {
+                width: 100%;
+                height: 100vh;
+                object-fit: cover;
+            }
+        }
+        .title_container {
+            width: 100%;
+            position: absolute;
+            top: 32%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: white;
+            .title {
+                font-size: 5vw;
+                font-weight: 700;
+                text-align: center;
+                text-shadow: 0 0 3rem rgba(0, 0, 0, 0.398);
+            }
+            .subTitle {
+                font-size: 2.5vw;
+                font-weight: 500;
+                text-align: center;
+                text-shadow: 0 0 1rem rgba(0, 0, 0, 0.398);
+                padding: 3.5em 0 1rem 0;
+            }
+            .description {
+                font-size: 1.3vw;
+                font-weight: 200;
+                text-align: center;
+                text-shadow: 0 0 1rem rgba(0, 0, 0, 0.398);
+            }
+            .spanColor {
+                color: #2e5339;
+            }
+        }
+    }
+}
+
+*/

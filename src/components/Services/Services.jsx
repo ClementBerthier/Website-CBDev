8b3f07558f15f0b43dd1be55fd8e3d8a0082534a
ClementBerthier <berthierclementpro@gmail.com>
2024-06-10 21:13:03 +0200

    sevices en cours

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -11,120 +11,130 @@ export default function Service() {
         navigation("/servicesPage", { state: { id } });
     };
     return (
-        <div className="services_container">
-            <h2 className="title">Nos services</h2>
-            <div className="services">
-                <div
-                    className="services_box topLeft"
-                    id="ConceptionSite"
-                    onClick={handleClick}
-                >
-                    <img
-                        className="services_logo"
-                        src="/images/laptop-dev-mode.svg"
-                        alt="conception web logo"
-                        id="ConceptionSite"
-                        onClick={handleClick}
-                    />
-                    <h2
-                        className="services_title"
+        <div className="services_global">
+            <div className="services_container">
+                <h2 className="title">Nos services</h2>
+                <div className="services">
+                    <div
+                        className="services_box topLeft"
                         id="ConceptionSite"
                         onClick={handleClick}
                     >
-                        Conception de sites web
-                    </h2>
-                    <p
-                        className="services_text"
-                        id="ConceptionSite"
-                        onClick={handleClick}
-                    >
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Fugiat, animi? Rerum iusto odio placeat nihil
-                        laborum aut ea fugit natus, id tempora perferendis amet
-                        blanditiis accusantium assumenda alias nesciunt quos.
-                    </p>
-                </div>
-                <div
-                    className="services_box topRight"
-                    id="ConceptionApp"
-                    onClick={handleClick}
-                >
-                    <img
-                        className="services_logo"
-                        src="/images/application.svg"
-                        alt="conception web logo"
-                        id="ConceptionApp"
-                        onClick={handleClick}
-                    />
-                    <h2
-                        className="services_title"
-                        id="ConceptionApp"
-                        onClick={handleClick}
-                    >
-                        Conception d{"'"}applications web
-                    </h2>
-                    <p
-                        className="services_text"
+                        <img
+                            className="services_logo"
+                            src="/images/laptop-dev-mode.svg"
+                            alt="conception web logo"
+                            id="ConceptionSite"
+                            onClick={handleClick}
+                        />
+                        <h2
+                            className="services_title"
+                            id="ConceptionSite"
+                            onClick={handleClick}
+                        >
+                            Conception de sites web
+                        </h2>
+                        <p
+                            className="services_text"
+                            id="ConceptionSite"
+                            onClick={handleClick}
+                        >
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Fugiat, animi? Rerum iusto odio placeat nihil
+                            laborum aut ea fugit natus, id tempora perferendis
+                            amet blanditiis accusantium assumenda alias nesciunt
+                            quos.
+                        </p>
+                    </div>
+                    <div
+                        className="services_box topRight"
                         id="ConceptionApp"
                         onClick={handleClick}
                     >
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Fugiat, animi? Rerum iusto odio placeat nihil
-                        laborum aut ea fugit natus, id tempora perferendis amet
-                        blanditiis accusantium assumenda alias nesciunt quos.
-                    </p>
-                </div>
-                <div
-                    className="services_box bottomLeft"
-                    id="Refonte"
-                    onClick={handleClick}
-                >
-                    <img
-                        className="services_logo"
-                        src="/images/tools.svg"
-                        alt="conception web logo"
-                        id="Refonte"
-                        onClick={handleClick}
-                    />
-                    <h2
-                        className="services_title"
+                        <img
+                            className="services_logo"
+                            src="/images/application.svg"
+                            alt="conception web logo"
+                            id="ConceptionApp"
+                            onClick={handleClick}
+                        />
+                        <h2
+                            className="services_title"
+                            id="ConceptionApp"
+                            onClick={handleClick}
+                        >
+                            Conception d{"'"}applications web
+                        </h2>
+                        <p
+                            className="services_text"
+                            id="ConceptionApp"
+                            onClick={handleClick}
+                        >
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Fugiat, animi? Rerum iusto odio placeat nihil
+                            laborum aut ea fugit natus, id tempora perferendis
+                            amet blanditiis accusantium assumenda alias nesciunt
+                            quos.
+                        </p>
+                    </div>
+                    <div
+                        className="services_box bottomLeft"
                         id="Refonte"
                         onClick={handleClick}
                     >
-                        Refonte & Maintenance
-                    </h2>
-                    <p className="services_text" id="Refonte">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Fugiat, animi? Rerum iusto odio placeat nihil
-                        laborum aut ea fugit natus, id tempora perferendis amet
-                        blanditiis accusantium assumenda alias nesciunt quos.
-                    </p>
-                </div>
-                <div
-                    className="services_box bottomRight"
-                    id="SEO"
-                    onClick={handleClick}
-                >
-                    <img
-                        className="services_logo"
-                        src="/images/stats-report.svg"
-                        alt="conception web logo"
-                        id="SEO"
-                        onClick={handleClick}
-                    />
-                    <h2
-                        className="services_title"
+                        <img
+                            className="services_logo"
+                            src="/images/tools.svg"
+                            alt="conception web logo"
+                            id="Refonte"
+                            onClick={handleClick}
+                        />
+                        <h2
+                            className="services_title"
+                            id="Refonte"
+                            onClick={handleClick}
+                        >
+                            Refonte & Maintenance
+                        </h2>
+                        <p className="services_text" id="Refonte">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Fugiat, animi? Rerum iusto odio placeat nihil
+                            laborum aut ea fugit natus, id tempora perferendis
+                            amet blanditiis accusantium assumenda alias nesciunt
+                            quos.
+                        </p>
+                    </div>
+                    <div
+                        className="services_box bottomRight"
                         id="SEO"
                         onClick={handleClick}
                     >
-                        Optimisation du référencement SEO
-                    </h2>
-                    <p className="services_text" id="SEO" onClick={handleClick}>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Fugiat, animi? Rerum iusto odio placeat nihil
-                        laborum aut ea fugit natus, id tempora perferendis amet
-                        blanditiis accusantium assumenda alias nesciunt quos.
-                    </p>
+                        <img
+                            className="services_logo"
+                            src="/images/stats-report.svg"
+                            alt="conception web logo"
+                            id="SEO"
+                            onClick={handleClick}
+                        />
+                        <h2
+                            className="services_title"
+                            id="SEO"
+                            onClick={handleClick}
+                        >
+                            Optimisation du référencement SEO
+                        </h2>
+                        <p
+                            className="services_text"
+                            id="SEO"
+                            onClick={handleClick}
+                        >
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Fugiat, animi? Rerum iusto odio placeat nihil
+                            laborum aut ea fugit natus, id tempora perferendis
+                            amet blanditiis accusantium assumenda alias nesciunt
+                            quos.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>

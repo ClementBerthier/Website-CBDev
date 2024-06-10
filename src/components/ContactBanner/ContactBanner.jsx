d430dbda9bc28baba18a68bd50b46533257887ef
ClementBerthier <berthierclementpro@gmail.com>
2024-06-10 21:33:28 +0200

    sevices en cours

--- src/components/ContactBanner/ContactBanner.jsx
+++ src/components/ContactBanner/ContactBanner.jsx
@@ -6,15 +6,17 @@ export default function ContactBanner() {
     return (
         <div className="contactBanner_container">
             <div className="contactBanner">
-                <h2 className="contactBanner_title">
-                    Discutons ensemble de votre projet.
-                </h2>
-                <p className="contactBanner_text">
-                    N{"'"}hésitez pas à nous contacter
-                </p>
-                <Link to="contact">
-                    <Button id="contact" text="Contact" />
-                </Link>
+                <div className="contactBanner_detail_container">
+                    <h2 className="contactBanner_title">
+                        Discutons ensemble de votre projet.
+                    </h2>
+                    <p className="contactBanner_text">
+                        N{"'"}hésitez pas à nous contacter
+                    </p>
+                    <Link to="contact">
+                        <Button id="contact" text="Contact" />
+                    </Link>
+                </div>
             </div>
         </div>
     );

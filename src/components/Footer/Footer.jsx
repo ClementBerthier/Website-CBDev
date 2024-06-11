90fb9070b046cd1532a1fca9574a5d34c94ca6d1
ClementBerthier <berthierclementpro@gmail.com>
2024-06-11 21:00:51 +0200

    footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,84 +10,119 @@ export default function Footer() {
             <div className="footer_container_section">
                 <div className="footer_navigation_container">
                     <div className="footer_section">
-                        <img
-                            className="footer_logo"
-                            src="/images/Logo_CBDev_white.png"
-                            alt="logo entreprise"
-                        />
-                        <p className="motto">
-                            Votre <span className="greenText">vision</span>,
-                            <br />
-                            notre <span className="greenText">création</span>
-                        </p>
-
-                        <h3 className="footer_title">Suivez nous </h3>
-                        <div className="footer_social_logo">
-                            <a className="footer_link" href="">
-                                <img src={facebook} alt="logo facebook" />
-                            </a>
-                            <a className="footer_link" href="">
-                                <img src={instagram} alt="logo instagram" />
-                            </a>
-                            <a className="footer_link" href="">
-                                <img src={linkedin} alt="logo instagram" />
-                            </a>
-                            <a
-                                className="footer_link"
-                                href="https://github.com/ClementBerthier"
-                                target="_blank"
-                                rel="noreferrer noopener"
-                                aria-label="github"
-                            >
-                                <img src={github} alt="logo github" />
-                            </a>
+                        <div className="footer_section_division">
+                            <img
+                                className="footer_logo"
+                                src="/images/Logo_CBDev_white.png"
+                                alt="logo entreprise"
+                            />
+                            <p className="motto">
+                                Votre <span className="greenText">vision</span>,
+                                notre{" "}
+                                <span className="greenText">création</span>
+                            </p>
                         </div>
-                    </div>
-                    <div className="footer_navigation">
-                        <div className="footer_navigation_section ">
-                            <h3 className="footer_navigation_title">
-                                Services
-                            </h3>
-                            <div className="footer_navigation_link_container">
-                                <a className="footer_navigation_link" href="">
-                                    Site
+                        <div className="footer_section_division_second">
+                            <h3 className="footer_title">Suivez nous </h3>
+                            <div className="footer_social_logo">
+                                <a className="footer_link" href="">
+                                    <img src={facebook} alt="logo facebook" />
                                 </a>
-                                <a className="footer_navigation_link" href="">
-                                    Application
+                                <a className="footer_link" href="">
+                                    <img src={instagram} alt="logo instagram" />
                                 </a>
-                                <a className="footer_navigation_link" href="">
-                                    Refonte
+                                <a className="footer_link" href="">
+                                    <img src={linkedin} alt="logo instagram" />
                                 </a>
-                                <a className="footer_navigation_link" href="">
-                                    Maintenance
-                                </a>
-                                <a className="footer_navigation_link" href="">
-                                    SEO
+                                <a
+                                    className="footer_link"
+                                    href="https://github.com/ClementBerthier"
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                    aria-label="github"
+                                >
+                                    <img src={github} alt="logo github" />
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <div className="footer_navigation ">
-                        <div className="footer_navigation_section links_section">
-                            <h3 className="footer_navigation_title">Liens</h3>
-                            <div className="footer_navigation_link_container">
-                                <a className="footer_navigation_link" href="">
-                                    Accueil
-                                </a>
-                                <a className="footer_navigation_link" href="">
-                                    A propos
-                                </a>
-                                <a className="footer_navigation_link" href="">
+                    <div className="footer_navigation_first">
+                        <div className="footer_navigation">
+                            <div className="footer_navigation_section ">
+                                <h3 className="footer_navigation_title">
                                     Services
-                                </a>
-                                <a className="footer_navigation_link" href="">
-                                    Contact
-                                </a>
+                                </h3>
+                                <div className="footer_navigation_link_container">
+                                    <a
+                                        className="footer_navigation_link"
+                                        href=""
+                                    >
+                                        Site
+                                    </a>
+                                    <a
+                                        className="footer_navigation_link"
+                                        href=""
+                                    >
+                                        Application
+                                    </a>
+                                    <a
+                                        className="footer_navigation_link"
+                                        href=""
+                                    >
+                                        Refonte
+                                    </a>
+                                    <a
+                                        className="footer_navigation_link"
+                                        href=""
+                                    >
+                                        Maintenance
+                                    </a>
+                                    <a
+                                        className="footer_navigation_link"
+                                        href=""
+                                    >
+                                        SEO
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="footer_navigation ">
+                            <div className="footer_navigation_section links_section">
+                                <h3 className="footer_navigation_title">
+                                    Liens
+                                </h3>
+                                <div className="footer_navigation_link_container">
+                                    <a
+                                        className="footer_navigation_link"
+                                        href=""
+                                    >
+                                        Accueil
+                                    </a>
+                                    <a
+                                        className="footer_navigation_link"
+                                        href=""
+                                    >
+                                        A propos
+                                    </a>
+                                    <a
+                                        className="footer_navigation_link"
+                                        href=""
+                                    >
+                                        Services
+                                    </a>
+                                    <a
+                                        className="footer_navigation_link"
+                                        href=""
+                                    >
+                                        Contact
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
+
                     <div className="footer_navigation ">
-                        <div className="footer_navigation_section">
+                        <div className="footer_navigation_section section_contact">
                             <h3 className="footer_navigation_title">Contact</h3>
                             <div className="footer_navigation_link_container">
                                 <div className="footer_navigation_link">

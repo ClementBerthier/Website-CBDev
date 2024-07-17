44f33e99ddf5b546cadefd70eb26e1c817e88120
ClementBerthier <berthierclementpro@gmail.com>
2024-07-17 12:14:37 +0200

    test without homeloader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "../Layout/Layout.jsx";
 import ServicesPage from "../ServicesPage/ServicesPage.jsx";
-import HomeLoader from "../HomeLoader/HomeLoader.jsx";
+//import HomeLoader from "../HomeLoader/HomeLoader.jsx";
 import LegalMention from "../LegalMention/LegalMention.jsx";
 
 export default function App() {
     return (
         <>
-            <HomeLoader />
+            {/*             <HomeLoader />
+             */}{" "}
             <Routes>
                 <Route path="/" element={<Layout />} />
                 <Route path="/servicesPage" element={<ServicesPage />} />
